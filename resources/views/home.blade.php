4b6eb36434c6c1752326296182632a7bb0a08638
winnie <winnie131212592@gmail.com>
2019-05-25 18:20:15 +0800

    refactor code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,22 +78,64 @@ function current_page($uri = "/") {
 
              <!--  <center><span id="display" style="font-size: 150px; color:#0a0;"><b>00:00</b></span><br><button type="button" class="btn btn-sm btn-danger" onclick="setCount()"><i class="glyphicon glyphicon-pause"></i> Pause</button><button type="button" class="btn btn-sm btn-success" onclick="setCount()"><i class="glyphicon glyphicon-play"></i> Start</button><button type="button" class="btn btn-sm btn-primary" onclick="setCount()"><i class="glyphicon glyphicon-arrow-right"></i> Next</button></center><br>
  -->
+              <center>
+                <h1 id="round" style="margin-bottom: -50px;">{{ $firstTournament->level }}</h1>
+                <div class="clock" style="font-size: 200px; color:#0a0; font-family:'digital-clock-font'"><b>{{ $duration->in_minutes }}</b></div>
+                
+                <div id="poker_blinds" style="margin-top: -50px; margin-bottom: 20px; font-size: 30px;">
+                  <div class="blinds">
+                    <span class="small-blind">{{ $blindParts['small'] }}</span>
+                    <span class="separator">/</span>
+                    <span class="big-blind">{{ $blindParts['big'] }}</span>
+                  </div>
+                  <span>Blinds</span>
+                </div>
 
-              <center><span id="clockdiv" style="font-size: 200px; color:#0a0; font-family:'digital-clock-font'"><b>00:00</b></span><br><button type="button" class="btn btn-sm btn-danger" id="pause"><i class="glyphicon glyphicon-pause"></i> Pause</button><button type="button" class="btn btn-sm btn-success" id="resume"><i class="glyphicon glyphicon-play"></i> Resume</button></center><br>
+                <br>
+                <button type="button" class="btn btn-sm btn-primary" id="poker_play_pause">
+                  <i class="glyphicon glyphicon-pause"></i> /
+                  <i class="glyphicon glyphicon-play"></i> 
+                  <span id="play_pause_div">Play</span>
+                </button>
+                <span style="margin-left: 50px;"></span>
+                <button type="button" class="btn btn-sm btn-success" id="poker_next_round"><i class="glyphicon glyphicon-pause"></i> Next Round</button>
+              </center>
+              <br>
+              <br>
+
+              <div class="pull-right">
+                <button type="button" class="btn btn-sm btn-warning reset"><i class="glyphicon glyphicon-pause"></i> Reset</button>
+              </div>
+              
+              <br>
+              <br>
               
               <audio id="soundHandle" style="display: none;"></audio>
 
-             <ul class="list-group">
-             <li class="list-group-item text-muted">CURRENT BLINDS: </li>
-             <li class="list-group-item text-right"><span class="pull-left"><span class="pull-left" style="font-size: 40px;"><strong>Level 1</strong></span></span><b style="font-size: 40px; color:#0a0;">25/50</b>
-            </li>
-
-            <li class="list-group-item text-muted">NEXT BLINDS:</li>
-             <li class="list-group-item text-right"><span class="pull-left"><span class="pull-left" style="font-size: 20px; color: gray;"><strong>Level 2</strong></span></span><b style="font-size: 25px; color:gray;">50/100</b>
-            </li>
-            </ul>  
- 
+              <ul class="list-group" id="tournament-timers">
+                @foreach ($tournaments as $tournament)
+                  <li class="list-group-item text-muted">NEXT BLINDS:</li>
+                  <li class="list-group-item text-right">
+                    <span class="pull-left">
+                      <span class="pull-left" style="font-size: 20px; color: gray;">
+                        <strong>{{ $tournament->level }}</strong>
+                      </span>
+                    </span><b style="font-size: 25px; color:gray;">{{ $tournament->blinds }}</b>
+                  </li>
+                @endforeach
+              </ul>  
         </form>
+
+         {{-- <li class="list-group-item text-muted">CURRENT BLINDS: </li>
+          <li class="list-group-item text-right">
+            <span class="pull-left">
+              <span class="pull-left" style="font-size: 40px;">
+                <strong>Level 1</strong>
+              </span>
+            </span>
+            <b style="font-size: 40px; color:#0a0;">25/50</b>
+          </li> --}}
+
 
     
         </div><!--/col-6-->
@@ -155,298 +197,190 @@ function current_page($uri = "/") {
               </div>
 
 
-
-
-
-
-
-
-
                
               </div><!--/tab-pane-->
           </div><!--/tab-content-->
 
     </div><!--/row-->
                                                       
- 
+<audio id="alarm" controls="controls">
+  <source src="{{asset('sound/alert.mp3')}}" type="audio/mpeg" />
+    Your browser does not support the audio element.
+  </audio>
 
 
 
-  
-<!-- 
-<script type="text/javascript">
-
-function setCount(){
-  var date = document.getElementById('date');
-  var time = document.getElementById('time');
- 
-  if(date.value == "" || time.value == ""){
-    alert("Please complete the required field!");
-  }else{
-    var countDate = new Date(date.value+" "+time.value).getTime();
- 
-    var countDay = new Date(date.value+" "+time.value).getDate();
- 
-      var x = setInterval(function(){
-      var nowTime = new Date().getTime();
-      var nowDate = new Date().getDate();
-      var distance = countDate - nowTime;
-      var dayDis = countDay - nowDate;
- 
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
- /*
-      document.getElementById("display").innerHTML = dayDis + "d " + hours + "h "
-      + minutes + "m " + seconds + "s ";*/
-
-      document.getElementById("display").innerHTML = "<b>"
-      + minutes + ":</b><b>" + seconds + "</b>";
- 
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("display").innerHTML = "<b><span class='tab blink' style='font-size: 150px; color:red;'>00:00</span></b>";
-
-  soundHandle.src = '{{asset('sound/alert.mp3')}}';
-  soundHandle.play();
-
-  setTimeout("location.reload(true);", 8000);
-
-      }
- 
-    }, 1000);
-  }
-}
-
-</script>   
- -->
-
-
-
+@push('custom-scripts')
 
 <script type="text/javascript">
-  
+  var Poker = (function () {
+    var round = 1;
+    var duration = '{{ $duration->in_seconds }}';
+    var timer = duration;
+        
+    {{-- {{ dd(json_encode($allBlinds)) }} --}}
 
-// 10 minutes from now
-var time_in_minutes = 1;
-var current_time = Date.parse(new Date());
-var deadline = new Date(current_time + time_in_minutes*60*1000);
+    var blinds = '{!! json_encode($allBlinds) !!}';
 
+    blinds = JSON.parse(blinds);
 
-function time_remaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
-  return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
-}
+    
 
-var timeinterval;
-function run_clock(id,endtime){
-  var clock = document.getElementById(id);
-  function update_clock(){
-    var t = time_remaining(endtime);
-    clock.innerHTML = '<b>'+t.minutes+':</b><b>'+t.seconds+'</b><b style="font-size: 20px; color:#0a0; font-family: digital-clock-font">Min. / Sec.</b>';
-    if(t.total<=0){ 
+    console.log(blinds);
 
-      clearInterval(timeinterval);
-
-      soundHandle.src = '{{asset('sound/alert.mp3')}}';
-      soundHandle.play();
-      document.getElementById("clockdiv").innerHTML = "<b><span class='tab blink' style='font-size: 200px ; color:red;'>00:00</span></b>";
-
-      setTimeout("location.reload(true);", 8000);
-     }
-  }
-  update_clock(); // run function once at first to avoid delay
-  timeinterval = setInterval(update_clock,1000);
-}
-run_clock('clockdiv',deadline);
-
-
-var paused = false; // is the clock paused?
-var time_left; // time left on the clock when paused
-
-function pause_clock(){
-  if(!paused){
-    paused = true;
-    clearInterval(timeinterval); // stop the clock
-    time_left = time_remaining(deadline).total; // preserve remaining time
-  }
-}
-
-function resume_clock(){
-  if(paused){
-    paused = false;
-
-    // update the deadline to preserve the amount of time remaining
-    deadline = new Date(Date.parse(new Date()) + time_left);
-
-    // start the clock
-    run_clock('clockdiv',deadline);
-  }
-}
-
-// handle pause and resume button clicks
-document.getElementById('pause').onclick = pause_clock;
-document.getElementById('resume').onclick = resume_clock;
-
-
-
-
-    var count = 1;
-    var countEl = document.getElementById("count");
-    function plus(){
-        count++;
-        countEl.value = count;
-    }
-    function minus(){
-      if (count >= 1) {
-        count--;
-        countEl.value = count;
-      }  
-    }
-
-
-    var count1 = 1;
-    var countEl1 = document.getElementById("count1");
-    function plus1(){
-        count1++;
-        countEl1.value = count1;
-    }
-    function minus1(){
-      if (count1 >= 1) {
-        count1--;
-        countEl1.value = count1;
-      }  
-    }
-
-
-
-</script>
-
-
-
-
-
-
-
-<script type="text/javascript">
-  
-
-
-//Add new prize
-$(document).ready(function(){
-
-  $('#addform').on('submit', function(e){
-    e.preventDefault();
-
-    $.ajax({
-      type: "POST".
-      url: "/addrecord",
-      data: $('#addform').serialize(),
-      success: function(response){
-        console.log(response)
-        $('#addformmodal').modal('hide')
-        alert("Data Saved");
-        //location.relaod();
+      
+    var interval_id;
+    
+    return {
+      isGamePaused: function () {
+        return !interval_id ? true : false;
       },
-      error: function(error){
-        console.log(error)
-        //alert("Data not saved");
+      playAlarm: function () {
+        $('#alarm')[0].play();
+      },
+      reset: function () {
+        // reset timer
+        this.resetTimer();
+        
+        this.stopClock();
+        
+        this.updateClock(timer);
+        
+        // reset play/pause button
+        this.updatePlayPauseButton();
+        
+        // reset round
+        round = 1;
+        
+        this.updateRound(round);
+        
+        // increase blinds
+        this.updateBlinds(round);
+      },
+      resetTimer: function () {
+        timer = duration;
+      },
+      startClock: function () {
+        var that = this;
+        
+        interval_id = setInterval(function () {
+          that.updateClock(timer);
+          
+          timer -= 1;
+        }, 1000);
+      },
+      startNextRound: function () {
+        // reset timer
+        this.resetTimer();
+        
+        this.stopClock();
+        
+        this.updateClock(timer);
+        
+        // reset play/pause button
+        this.updatePlayPauseButton();
+        
+        // increase round
+        round += 1;
+        
+        this.updateRound(round);
+        
+        // increase blinds
+        this.updateBlinds(round);
+      },
+      stopClock: function () {
+        clearInterval(interval_id);
+        interval_id = undefined;
+      },
+      updateBlinds: function (round) {
+        var round_blinds = blinds[round - 1] || blinds[blinds.length];
+        
+        $('.small-blind').html(round_blinds.small);
+        $('.big-blind').html(round_blinds.big);
+      },
+      updateClock: function (timer) {
+        var minute = Math.floor(timer / 60),
+            second = (timer % 60) + "",
+            second = second.length > 1 ? second : "0" + second;
+          
+        $('.clock').html(minute + ":" + second);
+        
+        if (timer <= 0) {
+          this.startNextRound();
+          
+          this.playAlarm();
+          
+          this.startClock();
+          
+          // update play/pause button
+          this.updatePlayPauseButton();
+        }
+      },
+      updatePlayPauseButton: function () {
+        var pause_play_button = $('#poker_play_pause a');
+        
+        if (this.isGamePaused()) {
+          pause_play_button.removeClass('pause');
+          pause_play_button.addClass('play');
+        } else {
+          pause_play_button.removeClass('play');
+          pause_play_button.addClass('pause');
+        }
+      },
+      updateRound: function (round) {
+        $('#round').html('Level' + ' ' + round);
       }
-    });
+    };
+  }());
+
+  $('#poker_play_pause').on('click', function (event) {
+    if (Poker.isGamePaused()) {
+      $('#poker_play_pause').removeClass('btn-primary');
+      $('#poker_play_pause').addClass('btn-danger');
+      $('#play_pause_div').html('Pause');
+
+      Poker.startClock();
+    } else {
+      blueButtonPlay();
+
+      Poker.stopClock();
+    }
+    
+    // update play/pause button
+    Poker.updatePlayPauseButton();
   });
-});
+
+  $('#poker_next_round').on('click', function (event) {
+    blueButtonPlay();
+    Poker.startNextRound();
+  });
+
+  $('body').on('keypress', function (event) {
+    if (Poker.isGamePaused()) {
+      Poker.startClock();
+    } else {
+      Poker.stopClock();
+    }
+    
+    // update play/pause button
+    Poker.updatePlayPauseButton();
+  });
 
 
+  $('.reset').on('click', function (event) {
+    if (confirm('Are you sure you want to reset?')){
+      blueButtonPlay();
+      Poker.reset();
+    }
+  });
+
+  function blueButtonPlay()
+  {
+    $('#poker_play_pause').removeClass('btn-danger');
+    $('#poker_play_pause').addClass('btn-primary');
+    $('#play_pause_div').html('Play');
+  }
 </script>
 
-
-
-<style type "text/css">
-<!--
-/* @group Blink */
-.blink {
-  -webkit-animation: blink .75s linear infinite;
-  -moz-animation: blink .75s linear infinite;
-  -ms-animation: blink .75s linear infinite;
-  -o-animation: blink .75s linear infinite;
-   animation: blink .75s linear infinite;
-}
-@-webkit-keyframes blink {
-  0% { opacity: 1; }
-  50% { opacity: 1; }
-  50.01% { opacity: 0; }
-  100% { opacity: 0; }
-}
-@-moz-keyframes blink {
-  0% { opacity: 1; }
-  50% { opacity: 1; }
-  50.01% { opacity: 0; }
-  100% { opacity: 0; }
-}
-@-ms-keyframes blink {
-  0% { opacity: 1; }
-  50% { opacity: 1; }
-  50.01% { opacity: 0; }
-  100% { opacity: 0; }
-}
-@-o-keyframes blink {
-  0% { opacity: 1; }
-  50% { opacity: 1; }
-  50.01% { opacity: 0; }
-  100% { opacity: 0; }
-}
-@keyframes blink {
-  0% { opacity: 1; }
-  50% { opacity: 1; }
-  50.01% { opacity: 0; }
-  100% { opacity: 0; }
-}
-
-
-
-
-.button {
-  margin: 0 0 0 5px;
-  text-indent: -9999px;
-  cursor: pointer;
-  width: 29px;
-  height: 29px;
-  float: left;
-  text-align: center;
-  background: url(buttons.png) no-repeat;
-}
-.dec {
-  background-position: 0 -29px;
-}
-
-.buttons {
-  padding: 20px 0 0 140px;
-}
-
-
-@font-face{
- font-family:'digital-clock-font-title';
- src: url('../../font/square_sans_serif_7.ttf');
-}
-
-
-@font-face{
- font-family:'digital-clock-font';
- src: url('../../font/digital-7 (mono).ttf');
-}
-
-
-/* @end */
- 
-</style>
-
-
-
-
-
- 
+@endpush
 @endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -203,5 +203,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <script src="{{ asset('adminlte/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
  -->
 
+  @stack('custom-scripts')
+
   </body>
 </html>
